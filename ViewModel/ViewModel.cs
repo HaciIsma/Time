c85ce7c4035ec2a1a5cc9bf4b2cf19095aa773c1
HaciIsma <Haciismayilov5@outlook.com>
2020-04-24 06:39:55 -0800

    first commit

---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Threading;
+using PropertyChanged;
+using System;
+
+namespace Time.ViewModel
+{
+    [AddINotifyPropertyChangedInterface]
+    public class ViewModel
+    {
+        public DispatcherTimer _timer;
+        public string CurrentTime { get; set; }
+        public ViewModel()
+        {
+            _timer = new DispatcherTimer(DispatcherPriority.Render);
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += (sender, args) =>
+            {
+                CurrentTime = DateTime.Now.ToLongTimeString();
+            };
+            _timer.Start();
+        }
+    }
+}
